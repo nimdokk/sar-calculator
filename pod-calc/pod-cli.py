90ca45ecf0643ec87cb9f6cdbb79fed884ec01c9
nimdokk@gmail.com <nimdokk@gmail.com>
2020-02-09 16:29:33 -0600

    CLI Updates

--- pod-calc/pod-cli.py
+++ pod-calc/pod-cli.py
@@ -4,21 +4,40 @@ import click
 #note, numbers do not need to be Metric, but for consistency, put them all in metric, do not mix Metric and Imperial
 
 @click.group()
-def pod_cli():
+def cli():
     """Obtain inputs and calculate PoD from Range of Detection with Correction Factors for Low, Medium, High Visibility"""
 
-@pod_cli.group('rd')
+#################### Range of Detection/AMDR calculator ###############################
+@cli.group('rd')
 def rd():
     """Calculate Range of Detection / AMDR"""
 
+@rd.command('rd_calc')
+def rd_calc():
+    print("Get the Estimated POD Percentage")
+    print("For consistency use the same distance measurements (recommend using metric)")
+    print("First get the Range of Detection / AMDR for the search assignment")
 
-@pod_cli.group('pod')
+    rd_list={"rd1":0,"rd2":0,"rd3":0,"rd4":0,"rd5":0,"rd6":0,"rd7":0,"rd8":0}
+
+    for range in rd_list:
+        rd_list[range]=float(input("Enter the distance in meters: "))
+
+    #Calculate the Range of Detection - AMDR from distance inputs
+    Rd=sum(rd_list.values())/len(rd_list.values())
+
+    print("Range of Detection/AMDR (Average meters)",round(Rd,2))
+    return(Rd)
+
+
+######################  pod calculator  ###########################
+@cli.group('pod')
 def pod():
-    """Calculate PoD"""
+    """Calculate PoD with Correction Factors"""
 
 @pod.command('pod_calc')
-
 def pod_calc():
+
     print("Get the Estimated POD Percentage")
     print("For consistency use the same distance measurements (recommend using metric)")
     print("First get the Range of Detection / AMDR for the search assignment")
@@ -33,12 +52,12 @@ def pod_calc():
 
     print("Range of Detection/AMDR (Average meters)",round(Rd,2))
 
+
     # the correction factors for low, medium, high visibility classes
     Cf={"low":1.1,"med":1.6,"high":1.8}
 
     # add something so that we could prompt to deterimine if this should eb based on searchers and time or GPS/GLONASS track distance
     #Obtain the TIme spent searching - this value should in Hours
-
     time_searching=float(input("Enter time searching (hours): "))
 
     #obtain the Velocity of the searchers in km/hr
@@ -64,6 +83,7 @@ def pod_calc():
 
     #print("\n")
 
+
     for key,value in Cf.items():
         #calculate sweep width for each Correction Factor
         W=Rd * Cf[key]
@@ -77,8 +97,8 @@ def pod_calc():
         #for completeness - calculate the Probability of Success = POA * POD
         #POS=POA*POD
         #print("POS Percentage for",key,"-visibility class is: ",format(POS,'.2%'))
-
+    return
 
 if __name__ == '__main__':
-    pod_cli()
+    cli()
 
